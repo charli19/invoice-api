4604236b8f3de824bdad652e137a51052e98ff64
Carles Saumell <carles.saumellmontes@winparf.com>
2024-05-01 20:31:48 +0200

    base project

---
+++ src/main/java/com/marosavat/invoiceapi/InvoiceapiApplication.java
@@ -0,0 +1,13 @@
+package com.marosavat.invoiceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marosavat/invoiceapi/InvoiceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marosavat.invoiceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
