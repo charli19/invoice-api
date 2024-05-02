17bb3452c591dba9c764a0ceb92db46b565ac48e
Carles Saumell <carles.saumellmontes@winparf.com>
2024-05-02 21:29:38 +0200

    resolve dns in railway

---
+++ src/main/java/com/marosavat/invoiceapi/configuration/WebClientConfiguration.java
@@ -0,0 +1,19 @@
+package com.marosavat.invoiceapi.configuration;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+public class WebClientConfiguration {
+    @Bean
+    @Primary
+    public WebClient webClient() {
+        HttpClient httpClient = HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
+}
